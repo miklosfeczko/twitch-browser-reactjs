b667098b76c8acadf50ff18b4546781c67fc4993
Miklos Feczko <miklosfeczko01@gmail.com>
2019-09-04 00:27:21 +0200

    Made a basic API call with fetch.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,15 @@ import './App.css';
 
 class App extends React.Component {
 
+componentDidMount = async () => {
+  const results = await fetch(`https://api.twitch.tv/helix/games/top`, {
+    headers: new Headers({
+      'Client-ID' : 'nj66gbe8njzhncv9x2ru7azb1g57iz'
+    })
+  });
+  console.log(results);
+}
+
 render() {
   return (
     <div>

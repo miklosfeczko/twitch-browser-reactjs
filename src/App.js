b8ed1df0dbc5af47fe4aced342c94722dad166b0
Miklos Feczko <miklosfeczko01@gmail.com>
2019-09-04 21:59:36 +0200

    Modified the fetch function to return a promise with the first 100 results and passed current state as props to TopGames component.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
 import React from 'react';
 import './App.css';
 
+import TopGames from './components/TopGames/TopGames'
+
 class App extends React.Component {
+state = {
+  TOPGAMES: []
+}
 
 componentDidMount = async () => {
-  const results = await fetch(`https://api.twitch.tv/helix/games/top`, {
+  const results = await fetch(`https://api.twitch.tv/helix/games/top?first=100`, {
     headers: new Headers({
       'Client-ID' : 'nj66gbe8njzhncv9x2ru7azb1g57iz'
     })
   });
   const data = await results.json();
   console.log(data);
+  this.setState({ TOPGAMES: data })
 }
 
 render() {
   return (
     <div>
       Hello from App!
+      <TopGames TOPGAMES={this.state.TOPGAMES}/>
     </div>
   )
 }
---
+++ src/components/TopGames/TopGames.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class TopGames extends Component {
+    render() {
+        return (
+            <div>
+                Hello from TopGames component!
+            </div>
+        )
+    }
+}
